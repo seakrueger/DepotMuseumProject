3e180ec9cfd7ff1f0871026703500b85f5285eae
Sean Krueger <sean2270@icloud.com>
2022-07-20 18:17:59 +0100

    nuclear main.py
    Rebase main

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import sys
 
 from hardware.buttons import ButtonHandler
 from Ui.video_handler import VideoHandler
+<<<<<<< HEAD
 from displays.display import Display 
 
 
 def main():
     display = Display(5)
+=======
+from displays.nuclear import Nuclear 
+
+
+def main():
+    display = Nuclear(5)
+>>>>>>> a639958 (nuclear main.py)
     
     q = Queue()
     buttons = ButtonHandler(display, q)
